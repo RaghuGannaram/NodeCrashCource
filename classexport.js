8be3e3f4182d2bb85e86d2c6970f7002119f9fd1
Raghu Gannaram <raghuvarangannaram@gmail.com>
2021-09-02 17:20:11 +0530

    Added exports and imports

--- classexport.js
+++ classexport.js
@@ -1,11 +1,18 @@
-class Person{
-    constructor(name,age){
-        this.name=name;
-        this.age=age;
-    }
-    greeting =function(){
-        console.log(`My name is ${this.name} and i am ${this.age}`);
-    }
-}
+// class Person{
+//     constructor(name,age){
+//         this.name=name;
+//         this.age=age;
+//     }
+//     greeting =function(){
+//         console.log(`My name is ${this.name} and i am ${this.age}`);
+//     }
+// }
 
-module.exports= Person;
+// module.exports= Person;
+
+
+const name = "John";
+
+// module.exports = name;
+module.exports.person = name;
+// export default name;
--- classimport.js
+++ classimport.js
@@ -1,4 +1,10 @@
-const Person =require("./classexport");
+// const Person =require("./classexport");
 
-var person=new Person("Raghu Gannaram",23);
-person.greeting();
+// var person=new Person("Raghu Gannaram",23);
+// person.greeting();
+
+// const name  = require('./classexport');
+const name  = require('./classexport').person;
+// import name from './classexport'
+
+console.log(name);
